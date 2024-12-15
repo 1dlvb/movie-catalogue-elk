9dd0a056e8501561ff447d05967c3d937fce74a5
Антон Матушкин <matushkin-2017@mail.ru>
2024-12-15 15:14:23 +0500

    chore: initial commit

---
+++ src/main/java/com/dlvb/moviecatalogueelk/MovieCatalogueElkApplication.java
@@ -0,0 +1,13 @@
+package com.dlvb.moviecatalogueelk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieCatalogueElkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieCatalogueElkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dlvb/moviecatalogueelk/MovieCatalogueElkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dlvb.moviecatalogueelk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCatalogueElkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
