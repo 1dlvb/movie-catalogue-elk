c335dc3b1c67eb7abd1fd4f3734db76c5b3cde48
Антон Матушкин <matushkin-2017@mail.ru>
2024-12-24 13:50:20 +0500

    feat: add fulltext functionality

--- src/main/java/com/dlvb/moviecatalogueelk/grpc/ElasticSearchService.java
+++ src/main/java/com/dlvb/moviecatalogueelk/grpc/ElasticSearchService.java
@@ -1,21 +1,41 @@
 package com.dlvb.moviecatalogueelk.grpc;
 
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.elasticsearch.core.SearchRequest;
+import co.elastic.clients.elasticsearch.core.SearchResponse;
+import co.elastic.clients.elasticsearch.core.search.Hit;
 import com.dlvb.graphqlmoviecatalogue.MovieSearchServiceGrpc;
 import com.dlvb.graphqlmoviecatalogue.Search;
+import com.dlvb.moviecatalogueelk.model.MovieDocument;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.grpc.stub.StreamObserver;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import net.devh.boot.grpc.server.service.GrpcService;
+import org.springframework.beans.factory.annotation.Value;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @GrpcService
+@RequiredArgsConstructor
 public class ElasticSearchService extends MovieSearchServiceGrpc.MovieSearchServiceImplBase {
+
+    @NonNull
+    private final ObjectMapper objectMapper;
+
+    @NonNull
+    private final ElasticsearchClient elasticsearchClient;
+
+    @Value("${spring.elasticsearch.index.name.movie}")
+    private String movieIndexName;
 
     @Override
     public void searchMovies(Search.SearchRequest request, StreamObserver<Search.SearchResponse> responseObserver) {
         String query = request.getQuery();
-
-        List<Search.Movie> movies = mockSearch(query);
+        List<Search.Movie> movies = performFullTextSearch(query);
 
         Search.SearchResponse.Builder responseBuilder = Search.SearchResponse.newBuilder();
         responseBuilder.addAllMovies(movies);
@@ -24,38 +44,38 @@ public class ElasticSearchService extends MovieSearchServiceGrpc.MovieSearchServ
         responseObserver.onCompleted();
     }
 
-    private List<Search.Movie> mockSearch(String query) {
+    private List<Search.Movie> performFullTextSearch(String query) {
         List<Search.Movie> movies = new ArrayList<>();
+        try {
+            SearchResponse<Map> searchResponse = elasticsearchClient.search(
+                    SearchRequest.of(s -> s
+                            .index(movieIndexName)
+                            .query(q -> q.multiMatch(m -> m
+                                    .fields("title", "description")
+                                    .query(query)
+                            ))
+                            .size(10)
+                    ),
+                    Map.class
+            );
 
-        if (query.contains("action")) {
-            movies.add(Search.Movie.newBuilder()
-                    .setId("1")
-                    .setTitle("Action Movie 1")
-                    .setGenreId("1")
-                    .setDescription("An exciting action movie.")
-                    .build());
-            movies.add(Search.Movie.newBuilder()
-                    .setId("2")
-                    .setTitle("Action Movie 2")
-                    .setGenreId("1")
-                    .setDescription("Another thrilling action movie.")
-                    .build());
-        } else if (query.contains("comedy")) {
-            movies.add(Search.Movie.newBuilder()
-                    .setId("3")
-                    .setGenreId("2")
-                    .setTitle("Comedy Movie 1")
-                    .setDescription("A hilarious comedy movie.")
-                    .build());
-        } else {
-            movies.add(Search.Movie.newBuilder()
-                    .setId("4")
-                    .setGenreId("3")
-                    .setTitle("Generic Movie")
-                    .setDescription("A generic movie with no specific genre.")
-                    .build());
+            for (Hit<Map> hit : searchResponse.hits().hits()) {
+                Map<String, Object> source = hit.source();
+                if (source != null) {
+                    MovieDocument movieDocument = objectMapper.convertValue(source, MovieDocument.class);
+
+                    Search.Movie.Builder movieBuilder = Search.Movie.newBuilder();
+                    movieBuilder.setId(String.valueOf(movieDocument.getId()));
+                    movieBuilder.setTitle(movieDocument.getTitle());
+                    movieBuilder.setDescription(movieDocument.getDescription());
+                    movieBuilder.setGenreId(String.valueOf(movieDocument.getGenreId()));
+
+                    movies.add(movieBuilder.build());
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
         return movies;
     }
 
